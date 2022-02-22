0ee2eda8a3cc488aeb5d11c7b7bb5a77c0fe19e0
Can UYUMAZ <58149556+tombensonnn@users.noreply.github.com>
2022-02-22 17:15:05 +0300

    “http-server”

--- http/index.js
+++ http/index.js
@@ -5,19 +5,15 @@ const server = http.createServer((req,res) => {
 
     if(url === '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>INDEX SAYFASI</h1>')
+        res.write('<h2>INDEX SAYFASI</h1>')
     }
     else if(url === '/hakkimda'){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>HAKKIMDA SAYFASI</h1>')
+        res.write('<h2>HAKKIMDA SAYFASI</h1>')
     }
     else if(url === '/iletisim'){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>ILETISIM SAYFASI</h1>')
-    }
-    else if(url === '/xxx'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h2>HOSGELDINIZ</h2>')
+        res.write('<h2>ILETISIM SAYFASI</h2>')
     }
     else{
         res.writeHead(404, {'Content-Type': 'text/html'});
